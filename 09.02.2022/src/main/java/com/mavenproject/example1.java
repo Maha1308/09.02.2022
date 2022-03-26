e61cf771c045f652c51ad5c75a18f0ebe089f099
MAHA <MAHA@DESKTOP-GUFMCJ1>
2022-03-26 11:13:18 +0530

    commit and push the file to github

---
+++ 09.02.2022/src/main/java/com/mavenproject/example1.java
@@ -0,0 +1,15 @@
+package com.mavenproject;
+
+public class example1 {
+
+	public static void main(String[] args) {
+     int  a= 45;
+     int  b= 11;
+     int s;
+     s=a+b;
+     
+     System.out.println("sum="+s);
+
+	}
+
+}
